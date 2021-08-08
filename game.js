70967c8d5221c0a62ac3a98d50dd9f727772e45a
Gavle-hub <erikdaniel650@gmail.com>
2021-08-08 14:19:55 -0700

    update 2.0

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ let personTurnCount = 0;
 let computerTurnCount = 0;
 let computerTurnTime;
 let resetColorTime;
+let computerRerunTimes = 0;
 red.addEventListener("click", function() {cubeClicked("red")});
 blue.addEventListener("click", function() {cubeClicked("blue")});
 yellow.addEventListener("click", function() {cubeClicked("yellow")});
@@ -25,62 +26,104 @@ green.addEventListener("click", function() {cubeClicked("green")});
 
 
 function startGame() {
+    
+    console.log("start game");
     computerTurn();
 }
 
-
-
-function computerTurn() {
-    removePlayerTurn();
-    clearTimeout(computerTurnTime);
-    yourPlays = [];
-    personTurnCount = 0;
-    rerunComputerPlays();
-    let randomNumber = Math.floor(Math.random() * (4 - 1) + 1);
-    let randomColor = colors[randomNumber];
- 
-     cubeClicked(randomColor);
-
-}
-
-function rerunComputerPlays() {
-     for(let i = 0; i < computerPlays.length; i ++) {
-
-       handleClickDesign(computerPlays[i], true);
-     }
-}
-
+//allow pressing on squares
 function yourTurn() {
+    
+    console.log("person turn");
     personTurnCount += 1;
     red.style.pointerEvents = "auto";
     blue.style.pointerEvents = "auto";
     yellow.style.pointerEvents = "auto";
     green.style.pointerEvents = "auto";
+    
 }
 
+//remove pressing on squares
 function removePlayerTurn() {
+    
+    console.log("removePlayerTurn");
     red.style.pointerEvents = "none";
     blue.style.pointerEvents = "none";
     yellow.style.pointerEvents = "none";
     green.style.pointerEvents = "none";
+    
 
 }
+
+
+//computer plays
+function computerTurn() {
+    
+    console.log("computerTurn");
+    removePlayerTurn();
+    clearTimeout(computerTurnTime);
+    
+    yourPlays = [];
+    
+    personTurnCount = 0;
+    
+     rerunComputerPlays();
+
    
+
+}
+
+function computerPlay() {
+    
+    computerRerunTimes = 0;
+    console.log("computerPlay");
+    let randomNumber = Math.floor(Math.random() * (4 - 1) + 1);
+    let randomColor = colors[randomNumber];
+    
+     cubeClicked(randomColor);
+     
+}
+//rerun plays of computer
+function rerunComputerPlays() {
+    
+    console.log("rerunComputerPlays");
+    if(computerPlays.length != 0){
+        console.log("rerun 1")
+        if(computerRerunTimes < computerPlays.length){
+            console.log("rerun 2")
+            handleClickDesign(computerPlays[computerRerunTimes], true);
+           
+        }
+        
+        else {
+            console.log("rerun 3")
+
+            computerPlay();
+        }
+    }
+    else {
+        console.log("Rerun 4");
+        computerPlay();
+    }
+}
+
+
+
+//press on square // picked a color
 function cubeClicked(color){
     
+    console.log("cubeclicked");
     handleClickMemory(color);
     handleClickDesign(color);
     removePlayerTurn();
-    console.log("computer Plays " + computerPlays); 
-    console.log("your Plays " + yourPlays);
-    console.log("turn " + personTurnCount);
     comparePlays();
-    
-     time = setTimeout( function() {resetColors(); handleTurn()} ,1000) 
+     time = setTimeout( function() {resetColors(false); handleTurn()} ,1000) 
 }
 
-
+// store clicks
 function handleClickMemory(color) {
+    
+    console.log("handleClickMemory");
     if(turn === "person"){
         yourPlays.push(color);
     }
@@ -91,19 +134,24 @@ function handleClickMemory(color) {
 }
 
 function handleClickDesign(color, rerun){
+    
+    console.log("handleClickDesign");
     window[color].style = "opacity: 50%";
 
 
-    if(rerun){
+    if(rerun){ 
+        computerRerunTimes += 1;
         clearTimeout(resetColorTime);
-       resetColorTime = setTimeout(1000, resetColors());
+       resetColorTime = setTimeout(3000, resetColors(rerun));
     }
 
 
 }
 
-
+// your pick vs computer pick
 function comparePlays() {
+    
+    console.log("comparePlays");
 
     if(computerPlays[personTurnCount - 1] != yourPlays[personTurnCount - 1]){
         gameOver();
@@ -122,18 +170,30 @@ function comparePlays() {
 }
 
 function gameOver() {
-    console.log("you lost");
+    
+    console.log("gameOVer");
 }
-
-function resetColors(color) {
+// reset colors back to original 
+function resetColors(rerun) {
+    
+    console.log("resetColors");
     red.style = "opacity: 100%";
     blue.style = "opacity: 100%";
     yellow.style = "opacity: 100%";
     green.style = "opacity: 100%";
+
+    console.log(rerun)
+    if(rerun){
+        rerunComputerPlays();
+    }
    
 }
-
+// whos turn it is after square picks
 function handleTurn() {
+    
+    console.log("handleTurn");
+    console.log("computer plays " + computerPlays);
+    console.log("your plays " + yourPlays);
     clearTimeout(time);
     if(turn === "computer"){
         turn = "person";
@@ -142,7 +202,7 @@ function handleTurn() {
     else {
         if(personTurnCount === computerPlays.length){
             turn = "computer";
-            computerTurnTime = setTimeout(200, computerTurn());
+            computerTurnTime = setTimeout(1000, computerTurn());
   
             
         }
